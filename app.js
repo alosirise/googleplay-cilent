eba6919d9cc70e220fcda96a395b12a9b67f92aa
Nantipat Suwanwong <scemisnis@gmail.com>
2019-11-26 03:07:44 +0700

    setting for standard design

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-$(function () {
- $("b").click(function(){
-     console.log(this.id);
- })   
-});
+// $(function () {
+//     $.get("http://localhost:8080/findall",function(data,status){
+//         console.log(status);
+//         console.log(data.length);
+//         data.forEach(element => {
+//             var item = ` <div class="card " style="width: 120px;  margin: 5px; ">
+//             <div class="card-body">
+
+//                 <b id="1" class="card-title">${element.name}</b>
+//             </div></div>`;
+//             $("#result").append(item);
+//         });
+//     });
+//  $("b").click(function(){
+//      console.log(this.id);
+//  })   
+// });
